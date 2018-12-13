78650b2945900eb3a4d8f06e5cf2e3476ad4d9f6
Mathias Horn <mathiashorn@web.de>
2018-12-13 11:16:24 +0100

    Server: User und UserRepository: Erweitert um Attribute, Getter und Setter bzw Methoden sowie Abgleich mit Variablennamen im SecurityPackage

--- server/src/main/java/de/academy/abschlussprojekt/herbie/server/user/User.java
+++ server/src/main/java/de/academy/abschlussprojekt/herbie/server/user/User.java
@@ -1,33 +1,59 @@
 package de.academy.abschlussprojekt.herbie.server.user;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private long Id;
 
+    @Column(unique = true)
     private String username;
+
     private String password;
+
+    private boolean isAdmin;
 
     public User() {
     }
 
-    public User(String username, String password) {
+    public User(String username, String password, boolean isAdmin) {
         this.username = username;
         this.password = password;
+        this.isAdmin = isAdmin;
+
+    }
+
+    public User(String username, String password) {
     }
 
     public String getUsername() {
         return username;
     }
 
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
